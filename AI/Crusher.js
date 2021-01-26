58c0770dd94b010d939fd3c1d2b739cc636cd216
TW <tezeuszw@gmail.com>
2021-01-26 21:58:20 +0100

    fix crash on unloaded ai

--- AI/Crusher.js
+++ AI/Crusher.js
@@ -119,6 +119,7 @@ function id(){//main function
 //create target selection
 //create ego wrapper
 //create moving functions/approaching/fleeing/waiting
+   if(self.uid == -1) return
    inputs();
    var o=get_objects();
    if(stage_clear){controller.keypress('right');controller.keypress('down');}
